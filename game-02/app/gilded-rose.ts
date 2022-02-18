b6af133b277b866dcfba60f12687a5105024b4d0
AlvaroJTorres <mangetrone@gmail.com>
2022-02-17 19:00:20 -0500

    Finished second exercise

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -3,7 +3,7 @@ export class Item {
     sellIn: number;
     quality: number;
 
-    constructor(name, sellIn, quality) {
+    constructor(name: string, sellIn, quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
@@ -18,52 +18,58 @@ export class GildedRose {
     }
 
     updateQuality() {
-        for (let i = 0; i < this.items.length; i++) {
-            if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if (this.items[i].quality > 0) {
-                    if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                        this.items[i].quality = this.items[i].quality - 1
-                    }
-                }
-            } else {
-                if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
-                    if (this.items[i].name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].sellIn < 11) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                        if (this.items[i].sellIn < 6) {
-                            if (this.items[i].quality < 50) {
-                                this.items[i].quality = this.items[i].quality + 1
-                            }
-                        }
-                    }
-                }
-            }
-            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                this.items[i].sellIn = this.items[i].sellIn - 1;
-            }
-            if (this.items[i].sellIn < 0) {
-                if (this.items[i].name != 'Aged Brie') {
-                    if (this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if (this.items[i].quality > 0) {
-                            if (this.items[i].name != 'Sulfuras, Hand of Ragnaros') {
-                                this.items[i].quality = this.items[i].quality - 1
-                            }
-                        }
-                    } else {
-                        this.items[i].quality = this.items[i].quality - this.items[i].quality
-                    }
-                } else {
-                    if (this.items[i].quality < 50) {
-                        this.items[i].quality = this.items[i].quality + 1
-                    }
-                }
-            }
-        }
+      updateQuality() {
+        this.items.forEach((item) => {
+          switch (true) {
+            case /.*Aged Brie.*/.test(item.name):
+              this.updateBrie(item)
+              break;
+            case /.*Backstage passes.*/.test(item.name):
+              this.updatePass(item)
+              break;
+            case /.*Conjured.*/.test(item.name):
+              this.updateConjured(item)
+              break;
+            case /.*Sulfuras.*/.test(item.name):
+              break;
+            default:
+              this.updateGeneral(item)
+              break;
+          }
+        });
+  
+        return this.items
+      }
+    }
 
-        return this.items;
+    private updateBrie(item: Item){
+      item.sellIn -= 1;
+      if (item.quality < 50) {
+        item.sellIn >= 0 ? (item.quality += 1) : (item.quality += 2);
+      }
+    }
+
+    private updatePass(item: Item){
+      item.sellIn -= 1;
+      if (item.quality < 50) {
+        if (item.sellIn <= 10 && item.sellIn > 5) item.quality += 2;
+        else if (item.sellIn <= 5 && item.sellIn >= 0) item.quality += 3;
+        else if (item.sellIn < 0) item.quality = 0;
+        else item.quality += 1;
+      }
+    }
+
+    private updateConjured(item: Item){
+      item.sellIn -= 1;
+      if (item.quality < 50) {
+        item.sellIn >= 0 ? (item.quality -= 2) : (item.quality -= 4);
+      }
+    }
+
+    private updateGeneral(item: Item){
+      item.sellIn -= 1;
+      if (item.quality < 50) {
+        item.sellIn >= 0 ? (item.quality -= 1) : (item.quality -= 2);
+      }
     }
 }
