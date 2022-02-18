0a68e291105b80426d9a67a8a2de94352704b6ff
AlvaroJTorres <mangetrone@gmail.com>
2022-02-18 14:47:19 -0500

    Solution modified according observations

--- game-02/app/gilded-rose.ts
+++ game-02/app/gilded-rose.ts
@@ -3,73 +3,84 @@ export class Item {
     sellIn: number;
     quality: number;
 
-    constructor(name: string, sellIn, quality) {
+    constructor(name, sellIn, quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 }
 
+export interface Item {
+  update(): void
+}
+
+Item.prototype.update = function() {
+  this.sellIn -= 1;
+  if (this.quality < 50) {
+    this.sellIn >= 0 ? (this.quality -= 1) : (this.quality -= 2);
+  }
+}
+
+export class Brie extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+
+  update() {
+    this.sellIn -= 1;
+    if (this.quality < 50) {
+      this.sellIn >= 0 ? (this.quality += 1) : (this.quality += 2);
+    }
+  }
+}
+
+export class Pass extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+
+  update() {
+    this.sellIn -= 1;
+    if (this.quality < 50) {
+      if (this.sellIn <= 10 && this.sellIn > 5) this.quality += 2;
+      else if (this.sellIn <= 5 && this.sellIn >= 0) this.quality += 3;
+      else if (this.sellIn < 0) this.quality = 0;
+      else this.quality += 1;
+    }
+  }
+}
+
+export class Conjured extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+
+  update() {
+    this.sellIn -= 1;
+    if (this.quality < 50) {
+      this.sellIn >= 0 ? (this.quality -= 2) : (this.quality -= 4);
+    }
+  }
+}
+
+export class Sulfuras extends Item {
+  constructor(name, sellIn, quality){
+    super(name, sellIn, quality)
+  }
+}
+
 export class GildedRose {
-    items: Array<Item>;
+  items: Array<Item>;
 
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
+  constructor(items = [] as Array<Item>) {
+    this.items = items;
+  }
 
-    updateQuality() {
-      updateQuality() {
-        this.items.forEach((item) => {
-          switch (true) {
-            case /.*Aged Brie.*/.test(item.name):
-              this.updateBrie(item)
-              break;
-            case /.*Backstage passes.*/.test(item.name):
-              this.updatePass(item)
-              break;
-            case /.*Conjured.*/.test(item.name):
-              this.updateConjured(item)
-              break;
-            case /.*Sulfuras.*/.test(item.name):
-              break;
-            default:
-              this.updateGeneral(item)
-              break;
-          }
-        });
+  updateQuality() {
+    this.items.forEach((item) => {
+    item.update()
+    });
   
-        return this.items
-      }
-    }
-
-    private updateBrie(item: Item){
-      item.sellIn -= 1;
-      if (item.quality < 50) {
-        item.sellIn >= 0 ? (item.quality += 1) : (item.quality += 2);
-      }
-    }
-
-    private updatePass(item: Item){
-      item.sellIn -= 1;
-      if (item.quality < 50) {
-        if (item.sellIn <= 10 && item.sellIn > 5) item.quality += 2;
-        else if (item.sellIn <= 5 && item.sellIn >= 0) item.quality += 3;
-        else if (item.sellIn < 0) item.quality = 0;
-        else item.quality += 1;
-      }
-    }
-
-    private updateConjured(item: Item){
-      item.sellIn -= 1;
-      if (item.quality < 50) {
-        item.sellIn >= 0 ? (item.quality -= 2) : (item.quality -= 4);
-      }
-    }
-
-    private updateGeneral(item: Item){
-      item.sellIn -= 1;
-      if (item.quality < 50) {
-        item.sellIn >= 0 ? (item.quality -= 1) : (item.quality -= 2);
-      }
-    }
+    return this.items
+  } 
 }
